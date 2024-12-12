16d62889e5bcf035bc5c26d6622d075e42b91427
ad9647 <ad9647@student.jamk.fi>
2024-12-12 21:08:20 +0300

    done

---
+++ script/script.js
@@ -0,0 +1,62 @@
+
+
+async function renderArtists() {
+    const artistsList = document.getElementById('artists-list')
+    const artistsSlides = document.getElementById('artists-slides')
+
+    const response = await fetch('http://localhost:5500/script/data/artists.json')
+    const artistsData = await response.json();
+
+    artistsData.data.forEach((artist,i) => {
+        
+        artistsList.innerHTML += `<a class="col d-flex card" data-index="${artist.id}" href="http://localhost:5500/artist.html?id=${artist.id}"> 
+                                    <div class="card artist-card w-100 rounded-0" style="background-image: url(${artist.photo_path});">
+                                        <div class="card-body text-center text-white">
+                                            <div class="display-2">
+                                                <h5 class="card-title display-2--desc">${artist.name}</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                  </a>`
+                                  
+        if(i === 0){
+            artistsSlides.innerHTML += `<a class="carousel-item active card" href="http://localhost:5500/artist.html?id=${artist.id}">
+                                            <img class="d-block w-100" src=${artist.photo_path} alt="${artist.name}">
+                                            <div class="carousel-caption">
+                                                <div class="display-2">
+                                                    <h5 class="card-title display-2--desc">${artist.name}</h5>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    `
+        }else{
+            artistsSlides.innerHTML += `<a class="carousel-item card" href="http://localhost:5500/artist.html?id=${artist.id}">
+                                            <img class="d-block w-100" src=${artist.photo_path} alt="${artist.name}">
+                                            <div class="carousel-caption">
+                                                <div class="display-2">
+                                                    <h5 class="card-title display-2--desc">${artist.name}</h5>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    `
+        }
+        
+
+        
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    renderArtists()
+
+    document.querySelectorAll('.menu-item').forEach(item => {
+        item.addEventListener('click', () => {
+          const offcanvas = document.querySelector('#offcanvasNavbar');
+          const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvas);
+          if (bsOffcanvas) {
+            bsOffcanvas.hide();
+          }
+        });
+    });
+})
+
